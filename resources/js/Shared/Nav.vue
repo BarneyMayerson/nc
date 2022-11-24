de8fb57c965e40c3d5dd944fbac3e5830cc0d022
Ian Travers <mr.itpe@gmail.com>
2022-11-24 15:14:53 +0300

    Installing Tailwind CSS

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,10 +1,10 @@
 <template>
-    <nav>
-        <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/settings">Settings</Link></li>
-        </ul>
-    </nav>
+  <nav>
+    <ul class="flex items-center max-w-lg mx-auto space-x-4">
+      <li><Link href="/" class="hover:underline">Home</Link></li>
+      <li><Link href="/settings" class="hover:underline">Settings</Link></li>
+    </ul>
+  </nav>
 </template>
 
 <script setup>
