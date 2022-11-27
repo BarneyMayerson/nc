8fb4b5e8adcf7889843b6072132464f0c93b6e95
Ian Travers <mr.itpe@gmail.com>
2022-11-27 10:16:24 +0300

    Extracting ToggleMode

--- resources/js/components/navigations/Nav.vue
+++ resources/js/components/navigations/Nav.vue
@@ -3,10 +3,12 @@
     <ul class="flex items-center max-w-lg mx-auto space-x-4">
       <li><Link href="/" class="hover:underline">Home</Link></li>
       <li><Link href="/settings" class="hover:underline">Settings</Link></li>
+      <li><ToggleMode /></li>
     </ul>
   </nav>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/inertia-vue3";
+import ToggleMode from "../shared/ToggleMode.vue";
 </script>
