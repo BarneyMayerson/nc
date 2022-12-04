34d63a3701e3974d1df0138355f07ea3728db3f2
Ian Travers <mr.itpe@gmail.com>
2022-12-04 11:08:02 +0300

    TopNav vue (ready example from tailwind ui) (wip)

--- resources/js/components/layouts/AppLayout.vue
+++ resources/js/components/layouts/AppLayout.vue
@@ -3,12 +3,13 @@
     <section
       class="p-6 bg-slate-100 dark:bg-slate-800 dark:text-white transition-colors duration-1000"
     >
-      <header class="flex items-center justify-between">
+      <TopNav />
+      <!-- <header class="flex items-center justify-between">
         <Logo class="w-28 h-16" />
         <div class="flex items-center">
           <Nav />
         </div>
-      </header>
+      </header> -->
     </section>
 
     <Container>
@@ -19,6 +20,7 @@
 
 <script setup>
 import Logo from "@/components/Logo.vue";
+import TopNav from "@/components/navigations/TopNav.vue";
 import Nav from "@/components/navigations/Nav.vue";
 import Container from "@/components/shared/Container.vue";
 </script>
