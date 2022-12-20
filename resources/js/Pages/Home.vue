c51927720460e673e83925d992dfd94d48f1b01d
Barney Mayerson <mr.barney.mayerson@gmail.com>
2022-12-20 15:22:12 +0300

    BaseInputVue (w/o Tests) VI

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -5,9 +5,28 @@
     <div class="mt-8">
       <PasswordInput />
     </div>
+
+    <div class="mt-8">
+      <input id="standart" />
+    </div>
+
+    <div class="mt-8">
+      <BaseInput
+        id="testing"
+        type="text"
+        label="Base Input"
+        v-model="firstName"
+        error="Nop enough"
+      />
+    </div>
+    <div class="mt-2">{{ firstName }}</div>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import PasswordInput from "@/components/shared/PasswordInput.vue";
+import BaseInput from "@/components/shared/BaseInput.vue";
+
+const firstName = ref("");
 </script>
---
+++ resources/js/components/shared/BaseInput.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :class="$attrs.class">
+    <label v-if="label" class="form-label" :for="id">{{ label }}</label>
+    <input
+      :id="id"
+      :type="type"
+      class="form-input"
+      :class="{ error: error }"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      v-bind="{ ...$attrs, class: null }"
+    />
+    <div v-if="error" class="form-error">{{ error }}</div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: "text",
+  },
+  label: {
+    type: [String, Boolean],
+  },
+  modelValue: {
+    type: [String, Number],
+    default: "",
+  },
+  error: {
+    type: [String, Boolean],
+  },
+});
+</script>
