aa35de8ea6ffd8c2622884a7dee397514fcbaa9e
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-01-09 10:46:31 +0300

    Extract auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,12 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-Route::middleware('guest')->group(function() {
-    Route::get('register', [RegisterController::class, 'create'])->name('register');
-    Route::post('register', [RegisterController::class, 'store']);
-
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
-});
-
-Route::middleware('auth')->group(function() {
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
-});
-
 
 Route::get('/', function () {
     sleep(1);
 
     return Inertia::render('Home');
 })->name('home');
+
+require __DIR__ . '/auth.php';
